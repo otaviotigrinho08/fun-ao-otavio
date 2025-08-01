4491bf88a8f41fb79c86c2e50d95cba7640aeddf
otaviotigrinho08 <160755457+otaviotigrinho08@users.noreply.github.com>
2025-08-01 08:10:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const caixaprincipal = document.querySelectorAll(".caixa-principal");
+const caixaperguntas = document.querySelectorAll("caixa-perguntas");
+const caixaAlternativas = document.querySelectorAll("caixaAlternativas");
+const caixaresultado = document.querySelectorAll("caixaresultado");
+const textoresultado = document.querySelectorAll("textoresultado");
+
+const perguntas = [
+{
+enunciado:"pergunta1",
+alternativas:[
+alternativa1,
+"alternativa2"
+]
+},
+{
+    enunciado:"pergunta2",
+    alternativas:[
+    alternativa1,
+    "alternativa2"
+    ]
+    },
+]
