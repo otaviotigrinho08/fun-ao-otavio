7725e1660beee07e3b02f57bc987b26b15daddcd
otaviotigrinho08 <160755457+otaviotigrinho08@users.noreply.github.com>
2025-08-20 08:44:10 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,82 @@
-const caixaPrincipal = document.querySelectorAll(".caixa-principal");
-const caixaPerguntas = document.querySelectorAll(".caixa-perguntas");
-const caixaAlternativas = document.querySelectorAll(".caixa-alternativas");
-const caixaResultado = document.querySelectorAll(".caixa-resultado");
-const textoResultado = document.querySelectorAll(".texto-resultado");
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
-{
-enunciado:"Pergunta 1",
-alternativas:["alternativa1","alternativa2"]
-},
-{
-enunciado:"Pergunta1",
-alternativas:["alternativa1","alternativa2"]
-},
-{
-enunciado:"Pergunta1",
-alternativas:["alternativa1","alternativa2"]
-},
-{
-enunciado:"Pergunta1",
-alternativas:["alternativa1","alternativa2"]
- },
-{
-enunciado:"Pergunta1",
-alternativas:["alternativa1","alternativa2"]
- }
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+     {
+        enunciado: "Pergunta 3",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 4",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 5",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
 ];
 
 let atual = 0;
 let perguntaAtual;
-function mostraPergunta(){
-perguntaAtual = perguntas [atual];
-caixaPerguntas.textContent = perguntaAtual.enunciado;
+let historiaFinal;       
+
+function mostraPergunta() {
+    if(atual>= perguntas.length){
+mostraResultado();
+return;
+
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent="";
+    mostraAlternativas();
 }
-mostraPergunta()
+
+function mostraAlternativas(){
+    for (const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        botaoAlternativas.addEventListener("click", ()=>respostaSelecionada(alternativa))  
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+} 
+
+mostraPergunta();
+
+function respostaSelecionada(opcaoSelecionada){
+const afirmacoes=opcaoSelecionada.afirmacoes;
+historiaFinal=afirmacoes;
+historiaFinal+=afirmacoes+"";
+atual++;
+mostraPergunta();
+
+
+
+
+}
